0498be3d344ee26b667b353d0e8c36a8bec78359
simon <shimsh16@gmail.com>
2019-03-20 09:05:23 +0200

    bestSellers

--- pageScripts/amazonBestSellers.js
+++ pageScripts/amazonBestSellers.js
@@ -1,12 +1,18 @@
 (async function  () {
     console.log('amazonBestSellers');
-    const mainList = document.getElementById('zg_browseRoot');
+    const mainList = document.getElementById('zg_browseRoot').getAttribute("ul");
 
     console.log(mainList);
 
     chrome.runtime.onMessage.addListener(
         function(request, sender, sendResponse) {
             console.log(request)
+
+            // TODO: get the right category
+
+            // TODO: Click on the category
+
+            // TODO: Send message 'categoryData'
         });
 /*    const message = {
         currentPage: 'affiliatePage'
